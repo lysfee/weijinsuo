92d56aab69d66ef2b73e504f660792523273d5b4
林玉山 <271536027@qq.com>
2018-11-30 18:15:51 +0800

    产品区

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 $(function(){
 	
+	/*计算导航长度*/
+	var ininiMobile=function(){
+		var $navTabs=$('.wjs_product .nav-tabs');
+		var liLength=0;
+		$navTabs.find('li').each(function(i,item){
+			var $li=$(this);//$(item)
+			liLength+=$li.outerWidth(true);
+			
+		});
+		$navTabs.width(liLength);
+		console.log(liLength);
+		new IScroll($('.nav-tabs-parent')[0],{
+			scrollX:true,
+			scrollY:false,
+			click:true
+		});
+		}
+	ininiMobile();
+
 })
